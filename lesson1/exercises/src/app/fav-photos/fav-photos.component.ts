126af53f50d7d46349896390bbe7ac1308965dec
Paul <pdeyarmin@gmail.com>
2020-04-17 13:39:26 -0400

    finished part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Maximum enjoyment located here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
+  image2 = 'https://img.buzzfeed.com/buzzfeed-static/static/2014-05/enhanced/webdr07/8/11/original-28778-1399564141-24.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*';
   image3 = '';
 
   constructor() { }
